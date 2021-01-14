850597e57c20862bc75ed668a008c2c95e9aefcd
ElisabethDuong <elisabeth.duong@gmail.com>
2021-01-14 10:09:27 +0100

    revised exercises 32-34

--- exercice33/js/main.js
+++ exercice33/js/main.js
@@ -9,3 +9,17 @@ $(document).ready(function () {
         })
     })
 });
+
+// on peut appliquer le code HTML que l'on veut sur les datas Ajax
+
+// $(document).ready(function () {
+//     $(".btn").click(function () {
+//         $.ajax({
+//             url: "https://restcountries.eu/rest/v2/name/france",
+//             success: function (data, status, response) {
+//                 $("#country").html(`<b>${data[0].name}</b>`);
+//                 $("#capital").html(`<i>${data[0].capital}</i>`);
+//             }
+//         })
+//     })
+// });
--- exercice34/js/main.js
+++ exercice34/js/main.js
@@ -1,18 +1,19 @@
-$(document).ready(function () {
-    $(".btn").click(function () {
+// $(document).ready(function () {
+//     $(".btn").click(function () {
 
-        var countryInput = "https://restcountries.eu/rest/v2/name/" + $("input").val();
+//         // var countryInput = "https://restcountries.eu/rest/v2/name/" + $("input").val();
+//         var countryInput = `https://restcountries.eu/rest/v2/name/" ${($("input").val())}`;
 
-        $.ajax({
-            url: countryInput,
-            success: function (data, status, response) {
-                $("#country").html(data[0].name);
-                $("#capital").html(data[0].capital);
-            }
-        })
+//         $.ajax({
+//             url: countryInput,
+//             success: function (data, status, response) {
+//                 $("#country").html(`<b>${data[0].name}</b>`);
+//                 $("#capital").html(`<i>${data[0].capital}</i>`);
+//             }
+//         })
 
-    })
-});
+//     })
+// });
 
 
 // Autre méthode en stockant dans des variables 
@@ -32,5 +33,21 @@ $(document).ready(function () {
 //             }
 //         })
 
+//     })
+// });
+
+
+// A REPRENDRE AVEC LE HTML
+
+// $(document).ready(function () {
+//     $(".btn").click(function () {
+//         $.ajax({
+//             url: `https://pokeapi.co/api/v2/pokemon/" ${($("input").val())}`,
+//             success: function (data, status, response) {
+//                 $("#pokemon").text(data.name);
+//                 $("#type").html(data.types[0].);
+//                 $("#type").html(data.types[0].);
+//             }
+//         })
 //     })
 // });
